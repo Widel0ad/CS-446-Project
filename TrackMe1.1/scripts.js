50cb086073834e8f2b18d78f82e68771e771e90b
Geoff W <worleyg@cwu.edu>
2016-05-25 21:56:35 -0700

    Added map markers, finished device info Div

--- TrackMe1.1/scripts.js
+++ TrackMe1.1/scripts.js
@@ -7,10 +7,27 @@ $(document).ready(function(){
 
 	$("#findItButton").click(function(){
 		$("#deviceInput").hide();
-		$("#UserID").text($("input[name=deviceID]"));
-		$("#UserMake").html("ooo");
-		$("#UserModel").html("asdasfasdfasdf");
-		$("#UserSerial").html("asdasfasdfasdf");
+		var input_1 = document.getElementById("deviceID").value;
+		document.getElementById('UserID').innerHTML = input_1;
+		var input_2 = document.getElementById("make").value;
+		document.getElementById('UserMake').innerHTML = input_2;
+		var input_3 = document.getElementById("model").value;
+		document.getElementById('UserModel').innerHTML = input_3;
+		var input_4 = document.getElementById("serial").value;
+		document.getElementById('UserSerial').innerHTML = input_4;
+
+		var DeviceMarker = new google.maps.Marker({
+				position:{lat:46.9969, lng:-120.5586},
+				map: map,
+				label: "aoaoaoaoaoa"
+			});
+
+		var infoWindow = new google.maps.InfoWindow({
+			map:map,
+			content: "Your Device"
+
+		});
+		infoWindow.setPosition(DeviceMarker.getPosition());
 	});
 });
 
